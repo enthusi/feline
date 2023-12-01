89e4b6cd2395b63132d7cac245ef055d8e1fb095
marvin <marvin.henschel@uni-potsdam.de>
2023-12-01 15:47:18 +0100

    reformated python

--- src/python3_codes_after_preprocessing/createnan_py3.py
+++ src/python3_codes_after_preprocessing/createnan_py3.py
@@ -1,37 +1,19 @@
-from mpdaf.obj import Cube
-from mpdaf.obj import Image
-
-import matplotlib.pyplot as plt
-from math import *
 import sys
-import numpy as np
+import mpdaf.obj
 
-first=True
-
-plot=True
+first = True
 
 if first:
-  name=sys.argv[1]
-  #infile=CubeDisk(name)
-  #infile.info()
-  #image=Cube(name,ext=4)
-  #image.write('image00.fits')
-  #sys.exit(0)
-  data=Cube(name,ext=1).sum(axis=0)
-  data.info()
-  print("data loaded")
-  stat=Cube(name,ext=2).sum(axis=0)
-  stat.info()
-  print("stat loaded")
-  #data.plot()
-  #raw_input('Press enter')
-  #stat.plot()
-  #raw_input('Press enter')
-  snimage=data.copy()
-  snimage.data=data.data/stat.data
-  snimage.info()
-  snimage.write('image00.fits')
-  
-else: 
-  snimage=Image('image00.fits')
-  
+	name = sys.argv[1]
+	data = mpdaf.obj.Cube(name, ext=1).sum(axis=0)
+	data.info()
+	print("data loaded")
+	stat = mpdaf.obj.Cube(name, ext=2).sum(axis=0)
+	stat.info()
+	print("stat loaded")
+	snimage = data.copy()
+	snimage.data = data.data / stat.data
+	snimage.info()
+	snimage.write('image00.fits')
+else:
+	snimage = mpdaf.obj.Image('image00.fits')
