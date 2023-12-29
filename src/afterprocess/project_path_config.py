8d67a31e1912090f4f699e83549ddcc30cf914d2
marvin <marvin.henschel@uni-potsdam.de>
2023-12-29 22:33:27 +0100

    now all paths are correctly identified on each operating system

--- src/afterprocess/project_path_config.py
+++ src/afterprocess/project_path_config.py
@@ -1,12 +1,6 @@
 import os
 
-current_level_directory = os.getcwd()
-parent_level_directory = os.path.abspath(os.path.join(current_level_directory, os.pardir))
-top_level_directory = os.path.abspath(os.path.join(parent_level_directory, os.pardir))
-
-PROJECT_ROOT = top_level_directory
-
-DATA_PATH_ROOT = "../../"
-DATA_PATH_LOOKUP = "../../data/lookup/"
-DATA_PATH_PROCESSED = "../../data/processed/"
-DATA_PATH_RAW = "../../data/raw/"
+DATA_PATH_ROOT = os.path.abspath("../../")
+DATA_PATH_LOOKUP = os.path.join(DATA_PATH_ROOT, "data", "lookup")
+DATA_PATH_PROCESSED = os.path.join(DATA_PATH_ROOT, "data", "processed")
+DATA_PATH_RAW = os.path.join(DATA_PATH_ROOT, "data", "raw")
