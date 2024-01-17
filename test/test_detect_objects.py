f49bc964536a1b573bdbcd985375605ea0ba074e
osoth <115990442+osoth@users.noreply.github.com>
2024-01-17 11:33:58 +0100

    Update test_detect_objects.py

--- test/test_detect_objects.py
+++ test/test_detect_objects.py
@@ -2,7 +2,12 @@ import numpy as np
 from feline.src.afterprocess.detect_objects import world_to_pix
 
 
-
+try:
+    import matplotlib.pyplot as plt
+    plt.switch_backend('agg')  # Use a non-interactive backend
+except ImportError:
+    pass
+    
 def test_world_to_pix():
     # Assuming you have a Coord object with wcs_world2pix method
     class MockCoord:
