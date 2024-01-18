15f82c90e484aeab9e248dcdedccd14332cf3663
marvin <marvin.henschel@uni-potsdam.de>
2024-01-18 12:47:23 +0100

    fixed file path

--- src/postprocessing/detect_objects.py
+++ src/postprocessing/detect_objects.py
@@ -10,7 +10,7 @@ import numpy as np
 import scipy
 import skimage
 import struct
-import feline.src.afterprocess.project_path_config as project_path_config
+import feline.src.postprocessing.project_path_config as project_path_config
 
 try:
     mpl.use("TkAgg")
