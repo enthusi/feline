9108b0cd28129b7303965a259317000dea078309
osoth <115990442+osoth@users.noreply.github.com>
2024-01-19 12:33:31 +0100

    Update detect_objects.py

--- src/postprocessing/detect_objects.py
+++ src/postprocessing/detect_objects.py
@@ -10,7 +10,7 @@ import numpy as np
 import scipy
 import skimage
 import struct
-import feline.src.postprocecssing.project_path_config as project_path_config
+import project_path_config
 
 try:
     mpl.use("TkAgg")
