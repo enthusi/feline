4cd5cee7c85ea2d61f2a792e674ab678a48d6c16
marvin <marvin.henschel@uni-potsdam.de>
2024-01-19 10:28:52 +0100

    moved `requirements.txt` to root directory, deleted dummy file, added docstrings to `create_masking_plot.py`, and added a new file that combines two files.

--- src/postprocessing/create_masking_plot.py
+++ src/postprocessing/create_masking_plot.py
@@ -5,10 +5,26 @@ import project_path_config
 
 
 def load_data(file, ext):
+    """
+    Load data from a file.
+
+    Parameters:
+    file (str): The file path.
+    ext (int): The extension number.
+
+    Returns:
+    mpdaf.obj.Cube: The sum of the cube data along the specified axis.
+    """
     return mpdaf.obj.Cube(file, ext=ext).sum(axis=0)
 
 
 def create_masking_plot():
+    """
+    Create a masking plot and write it to a file.
+
+    The function takes no parameters. It uses the filename from the command line arguments,
+    loads the data and statistic from the file, creates a signal-to-noise image, and writes it to 'image00.fits'.
+    """
     filename = sys.argv[1]
     file = os.path.join(project_path_config.DATA_PATH_RAW, filename)
 
