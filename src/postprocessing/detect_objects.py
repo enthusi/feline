4a74f8b79d0512feb114235cdf25e88da35a1980
osoth <115990442+osoth@users.noreply.github.com>
2024-01-19 14:01:42 +0100

    Update detect_objects.py

--- src/postprocessing/detect_objects.py
+++ src/postprocessing/detect_objects.py
@@ -10,10 +10,7 @@ import numpy as np
 import scipy
 import skimage
 import struct
-try:
-    import project_path_config
-except:
-    pass        
+import project_path_config   
 
 try:
     mpl.use("TkAgg")
