e9e48c1e21fe3de1c1d1d24ce630f983a4d34b1d
marvin <marvin.henschel@uni-potsdam.de>
2024-02-03 10:55:58 +0100

    some experiments

--- GUI.py
+++ GUI.py
@@ -1,21 +1,24 @@
-import tkinter as tk
 import datetime as dati
+import tkinter as tk
+import tkinter.ttk as ttk
 
 # main window
 window = tk.Tk()
 # window title
 window.title("feline")
-# open window maximised
+# Set a minimum window size (width x height)
+window.minsize(1920, 1080)
+# open window maximized
 window.attributes("-zoomed", True)
 
 label_text = tk.StringVar()
 label_text.set(f"Uhrzeit: {dati.datetime.now().strftime('%H:%M:%S')}")
-label = tk.Label(window, textvariable=label_text, font=("Helvetica", 14))
-button = tk.Button(window, text="Aktuelle Zeit anzeigen", command=lambda: label_text.set(f"Uhrzeit: {dati.datetime.now().strftime('%H:%M:%S')}"))
+label = ttk.Label(window, textvariable=label_text, font=("Helvetica", 14))
+button = ttk.Button(window, text="Aktuelle Zeit anzeigen", command=lambda: label_text.set(f"Uhrzeit: {dati.datetime.now().strftime('%H:%M:%S')}"))
 
 # Center the label using pack geometry manager
 label.pack(expand=True)
-button.pack()
+button.pack(expand=True)
 
 # build window
 window.mainloop()
