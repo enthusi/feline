0104b857156624b069eb244f189242d2eeda6b7f
soth1 <soth@uni-potsdam.de>
2024-05-15 12:32:04 +0200

    updated create_pdf

--- src/postprocessing/create_pdf.py
+++ src/postprocessing/create_pdf.py
@@ -12,7 +12,8 @@ def create_pdf_file():
 
 	for pdf_file in files_in_directory:
 		if pdf_file.endswith(".pdf"):
-			pdf_files.append(os.path.join(project_path_config.DATA_PATH_PDF, pdf_file))
+			pdf_files.append(os.path.join(project_path_config.DATA_PATH_PDF,
+			                              pdf_file))
 
 	for pdf_file in pdf_files:
 		merger.append(pdf_file)
@@ -27,9 +28,11 @@ def remove_pdf_files():
 	pdf_file_list = os.listdir(project_path_config.DATA_PATH_PDF)
 	for pdf_file in range(len(pdf_file_list)):
 		if "fig" in pdf_file_list[pdf_file]:
-			os.remove(os.path.join(project_path_config.DATA_PATH_PDF, pdf_file_list[pdf_file]))
+			os.remove(os.path.join(project_path_config.DATA_PATH_PDF,
+			                       pdf_file_list[pdf_file]))
 	# print("\033[91m All single PDF files successfully deleted! \033[0m")
 
 
-create_pdf_file()
-remove_pdf_files()
+if __name__ == "__main__":
+	create_pdf_file()
+	remove_pdf_files()
